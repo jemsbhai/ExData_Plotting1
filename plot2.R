9eb0d35d54b45a8073b34ebbe50030a2fb36152a
jemsbhai <jemsbhai@gmail.com>
2015-10-12 00:06:33 +0100

    project 1
    submitted files

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-drawplot1 <- function() {
+drawplot2 <- function() {
   dat1 <- read.csv2("household_power_consumption.txt", nrows = 200)
   dat2 <- read.csv2("household_power_consumption.txt",skip=grep("1/2/2007", readLines("household_power_consumption.txt")),nrows=2880, na.strings = "?")
   colnames(dat2) <- colnames(dat1)
@@ -6,8 +6,11 @@ drawplot1 <- function() {
   dat3$GAN <- as.character(dat3$Global_active_power)
   dat3$GAN <- as.numeric(dat3$GAN)
   dat3$Date2 <- as.Date(dat3$Date,"%d/%m/%Y")
-  hist(dat3$GAN, xlab = "Global Active Power (kilowatts)", col ="red", main = "Global Active Power")
-  dev.copy(png,'plot1.png')
+  dat3$dayofweek <- weekdays(dat3$Date2)
+  x <- paste(dat3$Date2, dat3$Time)
+  dat3$datetime <- strptime(x, "%Y-%m-%d %H:%M:%S")
+  plot(dat3$datetime,dat3$GAN, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+  dev.copy(png,'plot2.png')
   dev.off()
   
 }
